c3e14d417b95bf5259d97dddbf6fb4062d678aaa
ian2024 <ostapenkoyan@yahoo.com>
2022-11-08 21:04:23 +0200

    hooks fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+import { Container } from './App.styled';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+
+
 export const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    setContacts(parsedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+
+  const addContact = (name, number) => {
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    };
+     
+    const contactCard = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts([contactCard, ...contacts]);
+
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getFiltered = () => {
+    const nomalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact => contact.name.toLowerCase().includes(nomalizedFilter));
+  };
+
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+  
+  const filteredContact = getFiltered();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={filteredContact}
+        onDelete={deleteContact}
+      />
+    </Container>
   );
 };
+
+export default App;
